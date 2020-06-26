087e36d19babd1da7a71a80fbcd48e780f533bca
adeelkhan-ak <ak140175@gmail.com>
2020-06-26 06:29:51 -0700

    change

--- pkg.py
+++ pkg.py
@@ -1,6 +1,6 @@
-import math
+import mymath
 
-print(math.add(4,5))
-print(math.div(4, 2))
-print(math.mul(10, 5))
-print(math.squareroot(48))
+print(mymath.add(4,5))
+print(mymath.div(4, 2))
+print(mymath.mul(10, 5))
+print(mymath.squareroot(16))
